c8162499679d6e2c365055343e1a33dc60c0b1ce
Danilo Moreira <danilobenevides@live.com>
2026-01-21 20:06:22 +0000

    first steps login page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 
 export default function Index() {
   return (
-    <View>
-      <Text>Scan TCG</Text>
+    <View style={styles.container}>
+      <Image source={require("@/assets/img1.png")}
+      style={styles.illustration}
+      />
+      <Text style={styles.title}>Entrar</Text>
+      <Text style={styles.subtitle}>Acessar agora com e-mail e senha.</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FDFDFD',
+    padding: 32,
+  },
+  illustration: {
+    width: '100%',
+    height: 330,
+    resizeMode: 'contain',
+    marginTop: 62,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 900,
+  },
+  subtitle: {
+    fontSize: 16,
+
+
+  }
+})
+
